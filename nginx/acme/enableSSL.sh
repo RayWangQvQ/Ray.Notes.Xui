57fac5efe28995de2704fab6a7a47eb4e7060b4a
Ray <zai7lou@outlook.com>
2022-05-17 23:50:55 +0800

    update

--- nginx/acme/enableSSL.sh
+++ nginx/acme/enableSSL.sh
@@ -15,9 +15,10 @@ echo "Email:$TLS_EMAIL"
 echo -e "\n----------------更新包----------------"
 apt-get update
 
-echo -e "\n----------------安装cron crontab crontabs----------------"
+echo -e "\n----------------安装cron----------------"
 apt-get install -y cron
-apt-get install -y crontab crontabs
+# apt-get install -y crontab crontabs
+crontab -l
 
 echo -e "\n----------------安装acme.sh----------------"
 curl  https://get.acme.sh | sh -s email=$TLS_EMAIL
@@ -31,7 +32,7 @@ nginx -s reload
 
 echo -e "\n----------------复制证书----------------"
 mkdir /letsencrypt/$NGINX_HOST
-~/.acme.sh/acme.sh --install-cert -d $NGINX_HOST --key-file /letsencrypt/$NGINX_HOST/privkey.key  --fullchain-file /letsencrypt/$NGINX_HOST/fullchain.crt --reloadcmd "service nginx force-reload"
+~/.acme.sh/acme.sh --install-cert --force -d $NGINX_HOST --key-file /letsencrypt/$NGINX_HOST/privkey.key  --fullchain-file /letsencrypt/$NGINX_HOST/fullchain.crt --reloadcmd "service nginx force-reload"
 chmod 777 /letsencrypt/$NGINX_HOST
 
 echo -e "\n----------------新增tls的nginx配置----------------"
